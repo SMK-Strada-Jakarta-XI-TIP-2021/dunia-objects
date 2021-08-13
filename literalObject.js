ecbae3d4408875cfaeb509b9472614f17c80ea79
novi <novia@gmail.com>
2021-08-13 11:37:29 +0700

    tugas dunia.object novi

--- literalObject.js
+++ literalObject.js
@@ -1,1 +1,22 @@
 // Tulis kode di bawah ini
+// Membuat object literal
+let hewan = {
+    nama: "Kucing",
+    warna: "putih",
+    beratGram: 25,
+    bagus: true,
+    harga: 100000,
+    cekHarga: function () {
+        return this.nama + ' ini ' + ' harganya ' + this.harga + ' dengan berat ' + this.berat;
+    }
+};
+
+console.log(hewan);
+console.log(hewan.warna);
+console.log(hewan.nama);
+hewan.cekHarga();
+
+let cekHargaHewan = hewan.cekHarga();
+
+console.log('ini cek harga hewan ===', cekHargaHewan);
+
