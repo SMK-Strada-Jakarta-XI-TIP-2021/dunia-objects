503eadf6ce7d265d8760e77b30896f997beb281e
myairin <irenewindagultom@gmail.com>
2021-08-13 18:44:07 +0700

    revisi tugas dunia objects

--- literalObject.js
+++ literalObject.js
@@ -1,1 +1,16 @@
 // Tulis kode di bawah ini
+
+ class pohon {
+    constructor(nama, tinggi, family, kingdom) {
+      this.nama = nama;
+      this.tinggi = tinggi;
+      this.family = family;
+      this.kingdom = kingdom;
+    }
+}
+
+var pohon1 = new pohon("pohon Pinus", "2-4 meter", "Pinaceae", "-")
+console.log(pohon1);
+
+var pohon2 = new pohon("pohon Ketapang", "3 meter", "Combretaceae",  "Plantae Divisio")
+console.log(pohon2);
