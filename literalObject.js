74d958285fb452e746fa2af569f859b1e0fd3f4b
unknown <dwitegar611@gmail.com>
2021-08-13 22:27:57 +0700

    dunia object

--- literalObject.js
+++ literalObject.js
@@ -1,1 +1,23 @@
 // Tulis kode di bawah ini
+
+let sepeda = {
+    merk: "polygon",
+    size: "xl",
+    warna: "hitam putih",
+    kondisi: "sangat bagus",
+    harga: 80000000,
+
+    melajuKencang: function () {
+        return this.merk + ' melaju ' + ' kencang ' + 'ngenggg';
+    }
+};
+
+console.log(sepeda);
+console.log(sepeda.merk);
+console.log(sepeda.size);
+console.log(sepeda.melajuKencang());
+
+let sepedaMelajuKencang = sepeda.melajuKencang();
+
+console.log('ini ketika sepeda ' + sepedaMelajuKencang);
+
