e178860fd43c7abc01e89a2800aab9871fd93d08
gabriellenatt <gabriellenatasha525@gmail.com>
2021-08-16 17:06:11 +0700

    Tugas 1

---
+++ app.js
@@ -0,0 +1,16 @@
+// Tulis kode di bawah ini
+console.log("----------------TUGAS OBJECT LITERAL----------------");
+
+let binatang = {
+    hewan       : "anjing",
+    nama        : 'Barren',
+    ras         : 'Pitbull',
+    umur        : 1,
+    warnaBulu   : 'hitam',
+    }
+
+console.log("Ini adalah sekor " + binatang.hewan);
+console.log(`Namanya ${binatang.nama}, dia anjing ras ${binatang.ras}`);
+console.log(`Umurnya ${binatang.umur} tahun, bulunya berwarna ${binatang.warnaBulu}`);
+
+console.log("----------------------------------------------------");
