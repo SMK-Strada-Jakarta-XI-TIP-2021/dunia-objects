61fde2c4b0d7073ad292c77608dcd4e03da24c3a
jlerapatty <lerapattyjoaquin@gmail.com>
2021-08-18 08:52:15 +0700

    Joaquin

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # dunia-objects
 Membuat berbagai Object Literal dalam JavaScript
+// Tulis kode di bawah ini
+let buah = {
+    nama: "nanas",
+    warna: "kuning",
+    harga: 15000, 
+    beratGram: 650,
+    buahFresh: "true",
+    cekHarga: function () {
+        return this.nama + ' ini ' + ' harganya ' + this.harga + ' dengan berat ' + this.beratGram; // cek harga mobil
+    }
+};
+
+
+console.log(buah);
+console.log(buah.warna);
+console.log(buah.nama);
+console.log(buah.cekHarga());
+
+let cekHargabuah = buah.cekHarga();
+
+console.log('ini cek harga buah = ' + cekHargabuah);
--- literalObject.js
+++ literalObject.js
@@ -1,1 +1,31 @@
 // Tulis kode di bawah ini
+console.log("TUGAS DUNIA OBJECT")
+
+let buah = {
+    nama: "nanas",
+    warna: "kuning",
+    harga: 15000, 
+    beratGram: 650,
+    kondisi: "segar",
+    cekHarga: function () {
+        return this.nama + ' ini ' + ' harganya ' + this.harga + ' dengan berat ' + this.beratGram; 
+    }
+};
+
+// Pemanggilan Biasa
+console.log("===================")
+console.log("Pemanggilan Biasa")
+console.log("")
+console.log(buah);
+
+// Pemanggilan Chaining
+console.log("")
+console.log("===================")
+console.log("Pemanggilan Chaining")
+console.log("")
+console.log(buah.nama);
+console.log(buah.warna);
+console.log(`${buah.harga}`);
+console.log(`${buah.beratGram}`);
+console.log(buah.kondisi);
+console.log(buah.cekHarga());
